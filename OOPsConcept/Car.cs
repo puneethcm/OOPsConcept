480f6eb635e1ea3faf37ae314e6bdef9f039b43f
puneethcm <puneethkumarcm8050@gmail.com>
2023-03-14 20:00:59 +0530

    Object and Class

---
+++ OOPsConcept/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OOPsConcept
+{
+	public class Car
+	{
+		public string Color;
+		public void CarProperties(int price, string model)
+		{
+			Console.WriteLine("Car colour is " + Color);
+			Console.WriteLine( model + " car price is " + price);
+		}
+	}
+}
+
--- OOPsConcept/Program.cs
+++ OOPsConcept/Program.cs
@@ -1,8 +1,14 @@
-﻿internal class Program
+﻿using OOPsConcept;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to OOPs Concept");
+
+        Car car = new Car();
+        car.Color = "red";
+        car.CarProperties(8000, "VolksVagen");
         Console.ReadLine();
     }
 }
