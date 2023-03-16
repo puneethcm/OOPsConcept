5f73c5393217a8046ac4844799d7700b78445ddf
puneethcm <puneethkumarcm8050@gmail.com>
2023-03-16 16:16:41 +0530

    Variables

---
+++ OOPsConcept/Variables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace OOPsConcept
+{
+	public class Variables
+	{
+		const int a = 20;  // constant variable with fixed value
+		const int b = 10;
+		public void Add()
+		{
+			int result; // local variable
+			result = a + b;
+			Console.WriteLine("Result" + result);
+		}
+
+		static string place = "Banglore";
+		public static void Places()
+		{
+			Console.WriteLine("I live in " + place);
+		}
+	}
+}
+
