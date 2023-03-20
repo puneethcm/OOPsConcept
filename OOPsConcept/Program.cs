cc9a8cffde49911a5004b2569219589d4f82ea53
puneethcm <puneethkumarcm8050@gmail.com>
2023-03-20 17:29:11 +0530

    types of inheritance

--- OOPsConcept/Program.cs
+++ OOPsConcept/Program.cs
@@ -1,16 +1,38 @@
 ﻿using OOPsConcept;
+using static OOPsConcept.TypesOfInheritance;
+using static OOPsConcept.TypesOfInheritance.ClassB;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to OOPs Concept");
-
-        Car car = new Car();
-        Console.WriteLine("Car barnd is " + car.brand);
-        car.Type();
-        car.Color = "red";
-        car.CarProperties(800000, "Vento");
+        Console.WriteLine("Choose Type of Inheritance \n1:single Inheritance\n2:Hierarchical Inhritance\n3:MultiLevel Inheritance ");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                Car car = new Car();
+                Console.WriteLine("Car barnd is " + car.brand);
+                car.Type();
+                car.Color = "red";
+                car.CarProperties(800000, "Vento");
+                break;
+            case 2:
+                ClassA classA = new ClassA();
+                Console.WriteLine(classA.msg());
+                ClassB classB = new ClassB();
+                Console.WriteLine(classA.msg());
+                Console.WriteLine(classB.info());
+                ClassC classC = new ClassC();
+                Console.WriteLine(classA.msg());
+                Console.WriteLine(classC.getinfo());
+                break;
+            case 3:
+                Animal animal = new Animal();
+                animal.animal();
+                break;
+        }
         Console.ReadLine();
     }
 }
---
+++ OOPsConcept/TypesOfInheritance.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace OOPsConcept
+{
+	public class TypesOfInheritance
+	{
+        // hierarchical inheritance
+        public class ClassA  //base class
+        {
+            public string msg()
+            {
+                return "this is A class Method";
+            }
+        }
+        public class ClassB : ClassA
+        {
+            public string info()
+            {
+                msg();
+                return "this is B class Method";
+            }
+            public class ClassC : ClassA
+            {
+                public string getinfo()
+                {
+                    msg();
+                    return "this is C class Method";
+                }
+            }
+        }
+    }
+    // Melti Level inheritance
+    public class Person
+    {
+        public void person()
+        {
+             Console.WriteLine("this is the person class");
+        }
+    }
+    public class Bird : Person
+    {
+        public void bird()
+        {
+            person();
+            Console.WriteLine("this is the bird class");
+        }
+    }
+    public class Animal : Bird
+    {
+        public void animal()
+        {
+            person();
+            bird();
+            Console.WriteLine("this is the animal class");
+        }
+    }
+}
+
