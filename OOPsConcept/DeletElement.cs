36ecddb4058d3b606a1a554600a9687047246dbd
puneethcm <puneethkumarcm8050@gmail.com>
2023-03-21 12:40:06 +0530

    Delete Array

---
+++ OOPsConcept/DeletElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace OOPsConcept
+{
+	public class DeletElement
+	{
+		public static void DeleteElement(int[] arr, int delete)
+		{
+			int k = 0;
+			int[] result = new int[arr.Length - 1];
+
+			for(int i=0; i < arr.Length; i++)
+			{
+				if (delete != arr[i])
+				{
+					result[k] = arr[i];
+					k++;
+				}
+			}
+			foreach(int j in result)
+			{
+				Console.Write(j + " ");
+			}
+		}
+	}
+}
+
